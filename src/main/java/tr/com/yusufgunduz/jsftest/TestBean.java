933cf1e879b6a532a1a640009c18a1e7537d1dd4
Yusuf Gündüz <yusuf.gunduz@epias.com.tr>
2017-05-05 11:01:57 +0300

    initial commit

---
+++ src/main/java/tr/com/yusufgunduz/jsftest/TestBean.java
@@ -0,0 +1,54 @@
+package tr.com.yusufgunduz.jsftest;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+
+/**
+ * Created by Kambaa on 5/5/17.
+ */
+@ManagedBean
+@ViewScoped
+public class TestBean implements Serializable {
+
+    private String name;
+    private Integer age;
+
+    private Boolean formEditEnabled;
+
+    @PostConstruct
+    public void init() {
+        name = "Yusuf Gündüz";
+        age = 31;
+        formEditEnabled = Boolean.FALSE;
+    }
+
+    public void enableFormEditing() {
+        formEditEnabled = Boolean.TRUE;
+    }
+
+    public void saveData() {
+        formEditEnabled = Boolean.FALSE;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Boolean getFormEditEnabled() {
+        return formEditEnabled;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
